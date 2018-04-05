8097077e7440ba12b2a903eba850d51cb4d21a29
CanRau <cansrau@gmail.com>
2018-04-04 22:38:03 -0500

    TitledCopy content now html capable

--- src/components/TitledCopy.js
+++ src/components/TitledCopy.js
@@ -58,7 +58,7 @@ const TitledCopy = ({
         >
           {Array.isArray(content)
             ? content.map((x, i) => <div key={i}>{x}</div>)
-            : content
+            : <div dangerouslySetInnerHTML={{ __html: content }} />
           }
         </div>
       )}
