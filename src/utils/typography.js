d7ecc08dc9727c12c65575c3b3602170a46e0309
CanRau <cansrau@gmail.com>
2018-06-06 00:03:19 -0500

    FIX: fix theme headline font sizes & file cleanup
    headlines: increase font size for readability & consistency

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,69 +1,35 @@
 import Typography from 'typography'
-import {
-  colors,
-  fontFamilies,
-} from '@/theme'
+import { colors, fontFamilies } from '@/theme'
 
 export default new Typography({
   baseFontSize: `16px`,
   scaleRatio: 2.5,
   baseLineHeight: `1.55`,
-  blockMarginBottom: 1,
   bodyFontFamily: fontFamilies.main.split(`,`),
-  bodyGray: `20`,
-  bodyGrayHue: 0,
   bodyWeight: `100`,
-  boldWeight: 700,
   headerFontFamily: fontFamilies.accent.split(`,`),
-  headerGray: 20,
-  headerGrayHue: 0,
   headerWeight: `400`,
-  googleFonts: [
-    // {
-    //   name: `Cutive`,
-    //   styles: [ `400` ],
-    // }
-    // {
-    //   name: `Amatic SC`,
-    //   styles: [ `700` ],
-    // },
-    // {
-    //   name: `Quicksand`,
-    //   styles: [
-    //     `700`,
-    //     `700i`,
-    //     `400`,
-    //     `400i`,
-    //   ]
-    // }
-  ],
-  overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+  overrideStyles: () => ({
     '*, *::before, *::after': {
       boxSizing: `border-box`,
     },
-
     html: {
-      lineHeight: `1.15`,
       WebkitTextSizeAdjust: `100%`,
       MsTextSizeAdjust: `100%`,
       MsOverflowStyle: `scrollbar`,
       WebkitTapHighlightColor: `transparent`,
     },
-
-    body: {
-      margin: `0`,
-      fontSize: `1rem`,
-      fontWeight: `400`,
-      lineHeight: `1.5`,
-      textAlign: `left`,
-      backgroundColor: `#fff`,
-    },
-
+    'h1, h2, h3, h4, h5, h6': { letterSpacing: `.03rem` },
+    h1: { fontSize: `2.2rem` },
+    h2: { fontSize: `2rem` },
+    h3: { fontSize: `1.8rem` },
+    h4: { fontSize: `1.6rem` },
+    h5: { fontSize: `1.4rem` },
+    h6: { fontSize: `1.2rem` },
     a: {
       color: colors.link,
       textDecoration: `none`,
     },
-    
     'a:hover': {
       color: colors.linkHover,
       textDecoration: `none`,
