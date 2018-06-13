4ffed60000b50bd7969c458ad584492d0c8a220c
CanRau <cansrau@gmail.com>
2018-06-12 20:23:20 -0500

    FIX: custom scroll behavior
    * add shouldUpdateScroll hook to gatsby-browser to prevent gatsby from jumping to top
    * prevents GaiAma's scrolling to main-nav from flickering 👌

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,20 @@
-import 'babel-polyfill'
+// // require(`babel-polyfill`)
+
+// prevent gatsby from scrolling to top
+// if no hash included and action = push
+exports.shouldUpdateScroll = ({ pathname, prevRouterProps }) =>
+  pathname.includes(`#`) ||
+  (prevRouterProps && prevRouterProps.history.action !== `PUSH`)
 
 /* eslint-disable */
-exports.onRouteUpdate = ({ action, location }) => {
-  const scrollToMenu = () => {
-    const el = document.querySelector(`#main-nav`)
-    if (!el) return
-    const { top } = el.getBoundingClientRect()
-    window.scrollTo(0, top - 20)
-  }
+const scrollToMenu = () => {
+  const el = document.getElementById(`main-nav`)
+  if (!el) return
+  return window.scrollTo(0, el.offsetTop - 20)
+}
 
-  if (action === `PUSH` && !location.search) {
-    window.setTimeout(scrollToMenu, 1)
+exports.onRouteUpdate = ({ action, location }) => {
+  if (action === `PUSH` && !location.search && !location.hash) {
+    window.setTimeout(scrollToMenu, 10)
   }
 }
