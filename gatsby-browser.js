686b2a00ec89afb8e1ffa3cc23d70ae68b9a7611
CanRau <cansrau@gmail.com>
2018-06-26 21:52:47 -0500

    disable custom scroll behavior

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,19 +1,19 @@
-// // require(`babel-polyfill`)
-const idToJumpTo = `main-nav`
+// // // require(`babel-polyfill`)
+// const idToJumpTo = `main-nav`
 
-/* eslint-disable */
-const scrollToMenu = () => {
-  const el = document.getElementById(idToJumpTo)
-  if (el) return window.scrollTo(0, el.offsetTop - 20)
-}
+// /* eslint-disable */
+// const scrollToMenu = () => {
+//   const el = document.getElementById(idToJumpTo)
+//   if (el) return window.scrollTo(0, el.offsetTop - 20)
+// }
 
-// on scroll to top if no prevRouterProps
-exports.shouldUpdateScroll = ({ prevRouterProps }) => !prevRouterProps
+// // on scroll to top if no prevRouterProps
+// exports.shouldUpdateScroll = ({ prevRouterProps }) => !prevRouterProps
 
-// redirect to menu if action = PUSH
-// and either no # at all or hash = idToJumpTo
-exports.onRouteUpdate = ({ action, location: { hash } }) => {
-  if (action === `PUSH` && (!hash || hash === `#${idToJumpTo}`)) {
-    window.setTimeout(scrollToMenu, 10)
-  }
-}
+// // redirect to menu if action = PUSH
+// // and either no # at all or hash = idToJumpTo
+// exports.onRouteUpdate = ({ action, location: { hash } }) => {
+//   if (action === `PUSH` && (!hash || hash === `#${idToJumpTo}`)) {
+//     window.setTimeout(scrollToMenu, 10)
+//   }
+// }
