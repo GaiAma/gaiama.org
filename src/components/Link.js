fe8ebf11704bba6d31152b048c09263b4122a222
CanRau <cansrau@gmail.com>
2018-08-24 15:42:22 -0500

    chore: update custom scroll behaviour
    we like to jump to the menu on page navigations, excerpt for browser made ones like back button
    I had to fix this behaviour as reach/router does not (yet) provide the used action (POP, PUSH)
    using a quick method suggested by https://github.com/gatsbyjs/gatsby/issues/7454#issuecomment-415786239

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types'
 import QS from '@/utils/query-string'
 import { Link as GatsbyLink } from 'gatsby'
 
+if (typeof window !== `undefined`) {
+  // eslint-disable-next-line
+  window.__navigatingToLink = false
+}
+
 const Link = ({
   children,
   to,
@@ -30,7 +35,14 @@ const Link = ({
       {children}
     </a>
   ) : (
-    <GatsbyLink to={to} {...props}>
+    <GatsbyLink
+      to={to}
+      onClick={() => {
+        // eslint-disable-next-line
+        window.__navigatingToLink = true
+      }}
+      {...props}
+    >
       {children}
     </GatsbyLink>
   )
