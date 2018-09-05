d9972e3c2d87288f867aa78fdc9f4192d0ba2611
CanRau <cansrau@gmail.com>
2018-09-05 01:52:07 -0500

    fix: add missing gatsby-transformer-remark-multi-type plugin

---
+++ plugins/gatsby-transformer-remark-multi-type/extend-node-type.js
@@ -0,0 +1,464 @@
+const {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLEnumType,
+  GraphQLJSON,
+  GraphQLBoolean,
+} = require(`gatsby/graphql`)
+const Remark = require(`remark`)
+const select = require(`unist-util-select`)
+const sanitizeHTML = require(`sanitize-html`)
+const _ = require(`lodash`)
+const visit = require(`unist-util-visit`)
+const toHAST = require(`mdast-util-to-hast`)
+const hastToHTML = require(`hast-util-to-html`)
+const mdastToToc = require(`mdast-util-toc`)
+const Promise = require(`bluebird`)
+const prune = require(`underscore.string/prune`)
+const unified = require(`unified`)
+const parse = require(`remark-parse`)
+const stringify = require(`remark-stringify`)
+const english = require(`retext-english`)
+const remark2retext = require(`remark-retext`)
+const stripPosition = require(`unist-util-remove-position`)
+const hastReparseRaw = require(`hast-util-raw`)
+
+let fileNodes
+let pluginsCacheStr = ``
+let pathPrefixCacheStr = ``
+const astCacheKey = node =>
+  `transformer-remark-markdown-ast-${
+    node.internal.contentDigest
+  }-${pluginsCacheStr}-${pathPrefixCacheStr}`
+const htmlCacheKey = node =>
+  `transformer-remark-markdown-html-${
+    node.internal.contentDigest
+  }-${pluginsCacheStr}-${pathPrefixCacheStr}`
+const htmlAstCacheKey = node =>
+  `transformer-remark-markdown-html-ast-${
+    node.internal.contentDigest
+  }-${pluginsCacheStr}-${pathPrefixCacheStr}`
+const headingsCacheKey = node =>
+  `transformer-remark-markdown-headings-${
+    node.internal.contentDigest
+  }-${pluginsCacheStr}-${pathPrefixCacheStr}`
+const tableOfContentsCacheKey = node =>
+  `transformer-remark-markdown-toc-${
+    node.internal.contentDigest
+  }-${pluginsCacheStr}-${pathPrefixCacheStr}`
+
+// ensure only one `/` in new url
+const withPathPrefix = (url, pathPrefix) =>
+  (pathPrefix + url).replace(/\/\//, `/`)
+
+/**
+ * Map that keeps track of generation of AST to not generate it multiple
+ * times in parallel.
+ *
+ * @type {Map<string,Promise>}
+ */
+const ASTPromiseMap = new Map()
+
+module.exports = (
+  { type, store, pathPrefix, getNode, getNodes, cache, reporter },
+  pluginOptions
+) => {
+  if (type.name !== `MarkdownRemark` && !type.name.endsWith(`Markdown`)) {
+    return {}
+  }
+
+  pluginsCacheStr = pluginOptions.plugins.map(p => p.name).join(``)
+  pathPrefixCacheStr = pathPrefix || ``
+
+  return new Promise((resolve, reject) => {
+    // Setup Remark.
+    let remark = new Remark().data(`settings`, {
+      commonmark: true,
+      footnotes: true,
+      pedantic: true,
+    })
+
+    for (let plugin of pluginOptions.plugins) {
+      const requiredPlugin = require(plugin.resolve)
+      if (_.isFunction(requiredPlugin.setParserPlugins)) {
+        for (let parserPlugin of requiredPlugin.setParserPlugins(
+          plugin.pluginOptions
+        )) {
+          if (_.isArray(parserPlugin)) {
+            const [parser, options] = parserPlugin
+            remark = remark.use(parser, options)
+          } else {
+            remark = remark.use(parserPlugin)
+          }
+        }
+      }
+    }
+
+    async function getAST(markdownNode) {
+      const cacheKey = astCacheKey(markdownNode)
+      const cachedAST = await cache.get(cacheKey)
+      if (cachedAST) {
+        return cachedAST
+      } else if (ASTPromiseMap.has(cacheKey)) {
+        // We are already generating AST, so let's wait for it
+        return await ASTPromiseMap.get(cacheKey)
+      } else {
+        const ASTGenerationPromise = new Promise(async resolve => {
+          if (process.env.NODE_ENV !== `production` || !fileNodes) {
+            fileNodes = getNodes().filter(n => n.internal.type === `File`)
+          }
+          const ast = await new Promise((resolve, reject) => {
+            // Use Bluebird's Promise function "each" to run remark plugins serially.
+            Promise.each(pluginOptions.plugins, plugin => {
+              const requiredPlugin = require(plugin.resolve)
+              if (_.isFunction(requiredPlugin.mutateSource)) {
+                return requiredPlugin.mutateSource(
+                  {
+                    markdownNode,
+                    files: fileNodes,
+                    getNode,
+                    reporter,
+                    cache,
+                  },
+                  plugin.pluginOptions
+                )
+              } else {
+                return Promise.resolve()
+              }
+            }).then(() => {
+              const markdownAST = remark.parse(markdownNode.internal.content)
+
+              if (pathPrefix) {
+                // Ensure relative links include `pathPrefix`
+                visit(markdownAST, `link`, node => {
+                  if (
+                    node.url &&
+                    node.url.startsWith(`/`) &&
+                    !node.url.startsWith(`//`)
+                  ) {
+                    node.url = withPathPrefix(node.url, pathPrefix)
+                  }
+                })
+              }
+
+              // source => parse (can order parsing for dependencies) => typegen
+              //
+              // source plugins identify nodes, provide id, initial parse, know
+              // when nodes are created/removed/deleted
+              // get passed cached DataTree and return list of clean and dirty nodes.
+              // Also get passed `dirtyNodes` function which they can call with an array
+              // of node ids which will then get re-parsed and the inferred schema
+              // recreated (if inferring schema gets too expensive, can also
+              // cache the schema until a query fails at which point recreate the
+              // schema).
+              //
+              // parse plugins take data from source nodes and extend it, never mutate
+              // it. Freeze all nodes once done so typegen plugins can't change it
+              // this lets us save off the DataTree at that point as well as create
+              // indexes.
+              //
+              // typegen plugins identify further types of data that should be lazily
+              // computed due to their expense, or are hard to infer graphql type
+              // (markdown ast), or are need user input in order to derive e.g.
+              // markdown headers or date fields.
+              //
+              // wrap all resolve functions to (a) auto-memoize and (b) cache to disk any
+              // resolve function that takes longer than ~10ms (do research on this
+              // e.g. how long reading/writing to cache takes), and (c) track which
+              // queries are based on which source nodes. Also if connection of what
+              // which are always rerun if their underlying nodes change..
+              //
+              // every node type in DataTree gets a schema type automatically.
+              // typegen plugins just modify the auto-generated types to add derived fields
+              // as well as computationally expensive fields.
+              if (process.env.NODE_ENV !== `production` || !fileNodes) {
+                fileNodes = getNodes().filter(n => n.internal.type === `File`)
+              }
+              // Use Bluebird's Promise function "each" to run remark plugins serially.
+              Promise.each(pluginOptions.plugins, plugin => {
+                const requiredPlugin = require(plugin.resolve)
+                if (_.isFunction(requiredPlugin)) {
+                  return requiredPlugin(
+                    {
+                      markdownAST,
+                      markdownNode,
+                      getNode,
+                      files: fileNodes,
+                      pathPrefix,
+                      reporter,
+                      cache,
+                    },
+                    plugin.pluginOptions
+                  )
+                } else {
+                  return Promise.resolve()
+                }
+              }).then(() => {
+                resolve(markdownAST)
+              })
+            })
+          })
+
+          // Save new AST to cache and return
+          cache.set(cacheKey, ast)
+          // We can now release promise, as we cached result
+          ASTPromiseMap.delete(cacheKey)
+          return resolve(ast)
+        })
+        ASTPromiseMap.set(cacheKey, ASTGenerationPromise)
+        return await ASTGenerationPromise
+      }
+    }
+
+    async function getHeadings(markdownNode) {
+      const cachedHeadings = await cache.get(headingsCacheKey(markdownNode))
+      if (cachedHeadings) {
+        return cachedHeadings
+      } else {
+        const ast = await getAST(markdownNode)
+        const headings = select(ast, `heading`).map(heading => {
+          return {
+            value: _.first(select(heading, `text`).map(text => text.value)),
+            depth: heading.depth,
+          }
+        })
+
+        cache.set(headingsCacheKey(markdownNode), headings)
+        return headings
+      }
+    }
+
+    async function getTableOfContents(markdownNode) {
+      const cachedToc = await cache.get(tableOfContentsCacheKey(markdownNode))
+      if (cachedToc) {
+        return cachedToc
+      } else {
+        const ast = await getAST(markdownNode)
+        const tocAst = mdastToToc(ast)
+
+        let toc
+        if (tocAst.map) {
+          const addSlugToUrl = function(node) {
+            if (node.url) {
+              node.url = [pathPrefix, markdownNode.fields.slug, node.url]
+                .join(`/`)
+                .replace(/\/\//g, `/`)
+            }
+            if (node.children) {
+              node.children = node.children.map(node => addSlugToUrl(node))
+            }
+
+            return node
+          }
+          tocAst.map = addSlugToUrl(tocAst.map)
+
+          toc = hastToHTML(toHAST(tocAst.map))
+        } else {
+          toc = ``
+        }
+        cache.set(tableOfContentsCacheKey(markdownNode), toc)
+        return toc
+      }
+    }
+
+    async function getHTMLAst(markdownNode) {
+      const cachedAst = await cache.get(htmlAstCacheKey(markdownNode))
+      if (cachedAst) {
+        return cachedAst
+      } else {
+        const ast = await getAST(markdownNode)
+        const htmlAst = toHAST(ast, { allowDangerousHTML: true })
+
+        // Save new HTML AST to cache and return
+        cache.set(htmlAstCacheKey(markdownNode), htmlAst)
+        return htmlAst
+      }
+    }
+
+    async function getHTML(markdownNode) {
+      const cachedHTML = await cache.get(htmlCacheKey(markdownNode))
+      if (cachedHTML) {
+        return cachedHTML
+      } else {
+        const ast = await getHTMLAst(markdownNode)
+        // Save new HTML to cache and return
+        const html = hastToHTML(ast, {
+          allowDangerousHTML: true,
+        })
+
+        // Save new HTML to cache and return
+        cache.set(htmlCacheKey(markdownNode), html)
+        return html
+      }
+    }
+
+    const HeadingType = new GraphQLObjectType({
+      name: `MarkdownHeading${type.name}`,
+      fields: {
+        value: {
+          type: GraphQLString,
+          resolve(heading) {
+            return heading.value
+          },
+        },
+        depth: {
+          type: GraphQLInt,
+          resolve(heading) {
+            return heading.depth
+          },
+        },
+      },
+    })
+
+    const HeadingLevels = new GraphQLEnumType({
+      name: `HeadingLevels${type.name}`,
+      values: {
+        h1: { value: 1 },
+        h2: { value: 2 },
+        h3: { value: 3 },
+        h4: { value: 4 },
+        h5: { value: 5 },
+        h6: { value: 6 },
+      },
+    })
+
+    return resolve({
+      html: {
+        type: GraphQLString,
+        resolve(markdownNode) {
+          return getHTML(markdownNode)
+        },
+      },
+      htmlAst: {
+        type: GraphQLJSON,
+        resolve(markdownNode) {
+          return getHTMLAst(markdownNode).then(ast => {
+            const strippedAst = stripPosition(_.clone(ast), true)
+            return hastReparseRaw(strippedAst)
+          })
+        },
+      },
+      excerpt: {
+        type: GraphQLString,
+        args: {
+          pruneLength: {
+            type: GraphQLInt,
+            defaultValue: 140,
+          },
+          truncate: {
+            type: GraphQLBoolean,
+            defaultValue: false,
+          },
+        },
+        resolve(markdownNode, { pruneLength, truncate }) {
+          if (markdownNode.excerpt) {
+            return Promise.resolve(markdownNode.excerpt)
+          }
+          return getAST(markdownNode).then(ast => {
+            const excerptNodes = []
+            visit(ast, node => {
+              if (node.type === `text` || node.type === `inlineCode`) {
+                excerptNodes.push(node.value)
+              }
+              return
+            })
+            if (!truncate) {
+              return prune(excerptNodes.join(` `), pruneLength, `…`)
+            }
+            return _.truncate(excerptNodes.join(` `), {
+              length: pruneLength,
+              omission: `…`,
+            })
+          })
+        },
+      },
+      headings: {
+        type: new GraphQLList(HeadingType),
+        args: {
+          depth: {
+            type: HeadingLevels,
+          },
+        },
+        resolve(markdownNode, { depth }) {
+          return getHeadings(markdownNode).then(headings => {
+            if (typeof depth === `number`) {
+              headings = headings.filter(heading => heading.depth === depth)
+            }
+            return headings
+          })
+        },
+      },
+      timeToRead: {
+        type: GraphQLInt,
+        resolve(markdownNode) {
+          return getHTML(markdownNode).then(html => {
+            let timeToRead = 0
+            const pureText = sanitizeHTML(html, { allowTags: [] })
+            const avgWPM = 265
+            const wordCount = _.words(pureText).length
+            timeToRead = Math.round(wordCount / avgWPM)
+            if (timeToRead === 0) {
+              timeToRead = 1
+            }
+            return timeToRead
+          })
+        },
+      },
+      tableOfContents: {
+        type: GraphQLString,
+        resolve(markdownNode) {
+          return getTableOfContents(markdownNode)
+        },
+      },
+      // TODO add support for non-latin languages https://github.com/wooorm/remark/issues/251#issuecomment-296731071
+      wordCount: {
+        type: new GraphQLObjectType({
+          name: `wordCount${type.name}`,
+          fields: {
+            paragraphs: {
+              type: GraphQLInt,
+            },
+            sentences: {
+              type: GraphQLInt,
+            },
+            words: {
+              type: GraphQLInt,
+            },
+          },
+        }),
+        resolve(markdownNode) {
+          let counts = {}
+
+          unified()
+            .use(parse)
+            .use(
+              remark2retext,
+              unified()
+                .use(english)
+                .use(count)
+            )
+            .use(stringify)
+            .processSync(markdownNode.internal.content)
+
+          return {
+            paragraphs: counts.ParagraphNode,
+            sentences: counts.SentenceNode,
+            words: counts.WordNode,
+          }
+
+          function count() {
+            return counter
+            function counter(tree) {
+              visit(tree, visitor)
+              function visitor(node) {
+                counts[node.type] = (counts[node.type] || 0) + 1
+              }
+            }
+          }
+        },
+      },
+    })
+  })
+}
---
+++ plugins/gatsby-transformer-remark-multi-type/on-node-create.js
@@ -0,0 +1,71 @@
+const { basename } = require(`path`)
+const crypto = require(`crypto`)
+const grayMatter = require(`gray-matter`)
+const _ = require(`lodash`)
+
+module.exports = async function onCreateNode(
+  { node, getNode, loadNodeContent, actions, createNodeId },
+  pluginOptions
+) {
+  const { createNode, createParentChildLink } = actions
+
+  // We only care about markdown content.
+  if (
+    node.internal.mediaType !== `text/markdown` &&
+    node.internal.mediaType !== `text/x-markdown`
+  ) {
+    return
+  }
+
+  const content = await loadNodeContent(node)
+  let data = grayMatter(content, pluginOptions)
+
+  // Convert date objects to string. Otherwise there's type mismatches
+  // during inference as some dates are strings and others date objects.
+  if (data.data) {
+    data.data = _.mapValues(data.data, v => {
+      if (_.isDate(v)) {
+        return v.toJSON()
+      } else {
+        return v
+      }
+    })
+  }
+
+  const markdownNode = {
+    id: createNodeId(`${node.id} >>> MarkdownRemark`),
+    children: [],
+    parent: node.id,
+    internal: {
+      content: data.content,
+      type:
+        data.data && data.data.layout === `BlogPost`
+          ? `MarkdownRemark`
+          : node.relativePath && node.relativePath.includes(`newsletter`)
+            ? `newsletterMarkdown`
+            : _.upperFirst(_.camelCase(`${basename(node.dir)} Markdown`)),
+    },
+  }
+
+  markdownNode.frontmatter = {
+    title: ``, // always include a title
+    ...data.data,
+    _PARENT: node.id,
+  }
+
+  markdownNode.excerpt = data.excerpt
+  markdownNode.rawMarkdownBody = data.content
+
+  // Add path to the markdown file path
+  if (node.internal.type === `File`) {
+    markdownNode.fileAbsolutePath = node.absolutePath
+  }
+
+  markdownNode.internal.contentDigest = crypto
+    .createHash(`md5`)
+    .update(JSON.stringify(markdownNode))
+    .digest(`hex`)
+
+  createNode(markdownNode)
+  createParentChildLink({ parent: node, child: markdownNode })
+}
