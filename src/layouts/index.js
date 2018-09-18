177a8e918e0461edf1e9672bbd1d666fe52e9f8b
CanRau <cansrau@gmail.com>
2018-09-18 05:06:45 -0500

    style(Layout): add component name

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,8 @@
  * Passing data from Layout to Page / from Page to Layout
  * https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-plugin-layout
  */
-
 import React from 'react'
 
-export default ({ children }) => <>{children}</>
+const Layout = ({ children }) => <>{children}</>
+
+export default Layout
