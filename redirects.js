a09eb9265ce7a8cbd1df11e7d24ede0900463609
CanRau <cansrau@gmail.com>
2018-09-26 02:52:39 -0500

    fix(Pixel): proxy pixel to pixel.api..

--- redirects.js
+++ redirects.js
@@ -4,6 +4,7 @@ exports.redirects = [
   `https://gaiama.netlify.com/de/* https://www.gaiama.org/de/:splat 301!`,
   `https://gaiama.netlify.com/* https://www.gaiama.org/en/:splat 301!`,
   // Proxy /api to api.gaiama.org
+  `/api/pixel/* https://pixel.api.gaiama.org/:splat 200`,
   `/api/* https://api.gaiama.org/:splat 200`,
   // subdomain redirects
   `https://spende.gaiama.org/* https://www.gaiama.org/de/spenden/ 301!`,
