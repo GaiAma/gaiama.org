0c560e57d4de66ac468f2fe48f65d798ceb270dc
CanRau <cansrau@gmail.com>
2018-09-27 20:22:02 -0500

    fix: scroll behaviour, now it's a 2 liner
    probably due to upgrade to gatsby@2.0.10

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,27 +1,2 @@
-/* global window, document */
-const idToJumpTo = `main-nav`
-
-const scrollToMenu = () => {
-  const el = document.getElementById(idToJumpTo)
-  if (el) return window.scrollTo(0, el.offsetTop - 20)
-  return false
-}
-
-/**
- * fix custom scroll behaviour using __navigatingToLink declared in Link.js
- * by https://github.com/gatsbyjs/gatsby/issues/7454#issuecomment-415786239
- * as reach/router does not (yet) provide the used action for onRouteUpdate
- */
-export const shouldUpdateScroll = () => {
-  if (window.__navigatingToLink === true) {
-    return [0, 0]
-  }
-  return true
-}
-
-export const onRouteUpdate = () => {
-  if (window.__navigatingToLink === true) {
-    window.setTimeout(scrollToMenu, 10)
-  }
-  window.__navigatingToLink = false
-}
+export const shouldUpdateScroll = ({ prevRouterProps }) =>
+  prevRouterProps && prevRouterProps.location ? `main-nav` : true
