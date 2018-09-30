5a7811d02e15c611075f8ce12f1503abd7d1ed9b
CanRau <cansrau@gmail.com>
2018-09-29 20:55:33 -0500

    feat: add dev info to console & expose some details

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,32 @@
+import preval from 'babel-plugin-preval/macro'
+const { version, bugs } = preval`
+  const { version, bugs } = require('./package.json')
+  module.exports = { version, bugs }
+`
+
+try {
+  /* global window */
+  window.GaiAma = {
+    ...window.GaiAma,
+    version,
+    bugTracker: bugs,
+  }
+  console.log(
+    `%cWelcome to GaiAma.org version ${version}`,
+    `font-size:13px;color:#01422e;`
+  )
+  console.log(
+    `%cFeel free to inspect everything, e.g. 'window.GaiAma'`,
+    `color:green;`
+  )
+  console.log(
+    `%cIf you encounter anything unexpected, or have other feedback feel free to file an issue at ${
+      bugs.url
+    }`,
+    `color:#3a9a02;`
+  )
+  /* eslint-disable-next-line */
+} catch (e) {}
+
 export const shouldUpdateScroll = ({ prevRouterProps }) =>
   prevRouterProps && prevRouterProps.location ? `main-nav` : true
