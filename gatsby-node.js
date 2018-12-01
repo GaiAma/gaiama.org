0865f1924a9f7aa4727af4f47bc05ee2c8486970
CanRau <cansrau@gmail.com>
2018-12-01 14:22:37 -0500

    build: add babel plugins console & transform-remove-console

--- gatsby-node.js
+++ gatsby-node.js
@@ -459,4 +459,9 @@ exports.onCreateWebpackConfig = ({ actions, stage }) => {
 exports.onCreateBabelConfig = ({ actions: { setBabelPlugin } }) => {
   setBabelPlugin({ name: `@babel/plugin-proposal-optional-chaining` })
   setBabelPlugin({ name: `babel-plugin-transform-react-remove-prop-types` })
+  setBabelPlugin({ name: `babel-plugin-console` })
+  setBabelPlugin({
+    name: `babel-plugin-transform-remove-console`,
+    options: { exclude: [`log`] },
+  })
 }
