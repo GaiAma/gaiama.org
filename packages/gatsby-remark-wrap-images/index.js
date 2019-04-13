15824633266e0f2dcf98443b44f2bee9284d2cba
CanRau <cansrau@gmail.com>
2019-04-13 14:26:47 -0500

    build: quick fix gatsby-remark-wrap-images + cleanup
    add x.type !== `image`
    remove deprecated cheerio stuff
    and aspect-ratio helpers

--- packages/gatsby-remark-wrap-images/index.js
+++ packages/gatsby-remark-wrap-images/index.js
@@ -1,4 +1,3 @@
-// const cheerio = require(`cheerio`)
 const modifyChildren = require(`unist-util-modify-children`)
 
 module.exports = ({ markdownAST }, pluginOptions) => {
@@ -13,12 +12,32 @@ function modifier(node, index, parent) {
   if (parent.type !== `paragraph`) return false
 
   const whitespaceStripped = parent.children.filter(
-    x => !/^(\\n)*\s+$/.test(x.value)
+    x => !/^(\\n)*\s+$/.test(x.value) && x.type !== `image` // `type!=image` quick fix for some images not being transformed
   )
 
   const onlyContainsImages = whitespaceStripped.every(
     x => x.type === `html` && x.value.includes(`gatsby-resp-image-figure`)
   )
+
+  /**
+   * Debugging help
+   */
+  //   // parent.children.find(
+  //   //   x => x && x.value && x.value.includes(`IMG_9004`)
+  //   // ) &&
+  //   require(`fs`).writeFile(
+  //     `./InlineGallery.log`,
+  //     `\n\n
+  // [[parent.children]]:\n
+  // ${JSON.stringify(parent.children, null, 2)}
+  // \n
+  // [[whitespaceStripped]]:\n
+  // ${JSON.stringify(whitespaceStripped, null, 2)}
+  // [[onlyContainsImages]]:${JSON.stringify(onlyContainsImages, null, 2)}
+  //     \n\n`,
+  //     { flag: `a` },
+  //     err => err && console.log(err)
+  //   )
 
   if (onlyContainsImages) {
     parent.type = `div`
@@ -27,17 +46,6 @@ function modifier(node, index, parent) {
         class: `inline-gallery`,
       },
     }
-    // parent.children.map(x => {
-    //   const $ = cheerio.load(x.value)
-    //   const imageWithRatio = $(`[data-ratio]`)
-    //   if (imageWithRatio) {
-    //     if (parent.children.length > 1) {
-    //       imageWithRatio.css(`flex`, imageWithRatio.attr(`data-ratio`))
-    //     }
-    //     imageWithRatio.attr(`data-ratio`, null)
-    //     x.value = $(`body`).html()
-    //   }
-    // })
     parent.children = whitespaceStripped
   }
 
