8689d2fb05677579b55b3ad36dc804d193e4bfcb
CanRau <cansrau@gmail.com>
2019-04-14 03:54:16 -0500

    build: add disabled why-did-you-update to layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,14 @@
  */
 import React from 'react'
 
-const Layout = ({ children }) => <>{children}</>
+// if (process.env.NODE_ENV !== `production`) {
+//   const { whyDidYouUpdate } = require(`why-did-you-update`)
+//   whyDidYouUpdate(React, {
+//     collapseComponentGroups: false,
+//     // include: [/^Input$/],
+//   })
+// }
+
+const Layout = ({ children }) => children
 
 export default Layout
