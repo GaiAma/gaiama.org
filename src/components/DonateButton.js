390e3b4933c98cb7c5000aa8aefc97828d768f8e
CanRau <cansrau@gmail.com>
2019-12-12 19:35:23 -0500

    fix(DonateButton): paymentOptions spacing

--- src/components/DonateButton.js
+++ src/components/DonateButton.js
@@ -9,15 +9,36 @@ import { faCcAmex } from '@fortawesome/free-brands-svg-icons/faCcAmex'
 import Link from '@components/Link'
 
 export const DonateButton = ({ paymentOptions = false, ...props }) => (
-  <div>
-    <Link {...props} />
+  <div
+    sx={{
+      display: `flex`,
+      maxWidth: `14rem`,
+      flexDirection: `column`,
+      margin: `0 auto`,
+    }}
+  >
+    <Link {...props} sx={{ display: `block` }} />
 
     {!!paymentOptions && (
-      <div sx={{ display: `flex` }}>
-        <FontAwesomeIcon icon={faPaypal} />
-        <FontAwesomeIcon icon={faCcVisa} />
-        <FontAwesomeIcon icon={faCcMastercard} />
-        <FontAwesomeIcon icon={faCcAmex} />
+      <div
+        sx={{
+          display: `flex`,
+          justifyContent: `space-between`,
+          marginTop: `1rem`,
+        }}
+      >
+        <FontAwesomeIcon icon={faPaypal} size="lg" />
+        <FontAwesomeIcon
+          icon={faCcVisa}
+          size="lg"
+          sx={{ margin: `0 1rem 0` }}
+        />
+        <FontAwesomeIcon
+          icon={faCcMastercard}
+          size="lg"
+          sx={{ margin: `0 1rem 0` }}
+        />
+        <FontAwesomeIcon icon={faCcAmex} size="lg" />
       </div>
     )}
   </div>
