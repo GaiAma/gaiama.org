5fc73e6002fd403ba2f784e630a71ddf688f16cd
CanRau <cansrau@gmail.com>
2019-12-15 12:54:25 -0500

    feat(SupportPage): updates + cleanup

--- src/templates/SupportPage.js
+++ src/templates/SupportPage.js
@@ -2,22 +2,18 @@
 import React from 'react'
 import { jsx } from 'theme-ui'
 import PropTypes from 'prop-types'
-import { graphql, Link } from 'gatsby'
+import { Box, Heading, Text } from '@theme-ui/components'
+import { graphql } from 'gatsby'
 import Img from 'gatsby-image/withIEPolyfill'
 import { Link } from '@components/Link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faNewspaper } from '@fortawesome/free-solid-svg-icons/faNewspaper'
 // import { faRssSquare } from '@fortawesome/free-solid-svg-icons/faRssSquare'
 import MainLayout from '@components/MainLayout'
 import { SupportWidget } from '@components/Shared'
-import CheckMark from '@src/assets/check.png'
 import TitledCopy from '@components/TitledCopy'
-import { colors, fullPageWidth, media, visible } from '../theme'
+import { media, visible } from '@src/theme'
+import { DonateButton } from '@components/DonateButton'
 
-const ContributorListTitle = props => (
-  /* eslint-disable-next-line */
-  <h2 sx={{ margin: `2rem 0 0` }} {...props} />
-)
 const ContributorList = props => (
   <div
     sx={{
@@ -106,12 +102,22 @@ const SupportPage = props => {
   ))
 
   return (
-    <MainLayout {...props}>
+    <MainLayout
+      {...props}
+      wrapperStyles={{
+        width: `100%`,
+        maxWidth: `100%`,
+        pt: 0,
+      }}
+    >
       <TitledCopy
         rank="1"
         centered
         title={frontmatter.intro.title_}
         sx={{
+          mx: `auto`,
+          maxWidth: `50rem`,
+          mt: `3rem`,
           [media.lessThan(`medium`)]: {
             '& > h2': {
               fontSize: `2rem`,
@@ -125,6 +131,8 @@ const SupportPage = props => {
 
       <div
         sx={{
+          mx: `auto`,
+          maxWidth: `50rem`,
           display: `flex`,
           justifyContent: `space-around`,
           [media.lessThan(`medium`)]: {
@@ -150,6 +158,8 @@ const SupportPage = props => {
 
       <div
         sx={{
+          mx: `auto`,
+          maxWidth: `50rem`,
           display: `flex`,
           [media.lessThan(`medium`)]: {
             flexDirection: `column`,
@@ -166,6 +176,8 @@ const SupportPage = props => {
           sx={{
             border: `none`,
             position: `relative`,
+            display: `flex`,
+            flexDirection: `column`,
             [media.lessThan(`medium`)]: {
               marginBottom: `2rem`,
             },
@@ -180,7 +192,8 @@ const SupportPage = props => {
               display: `block`,
               right: 0,
               bottom: 0,
-              background: `linear-gradient( to right, ${colors.gray4}, ${colors.gray8}, ${colors.gray4}) no-repeat`,
+              background: theme =>
+                `linear-gradient( to right, ${theme.colors.gray4}, ${theme.colors.gray8}, ${theme.colors.gray4}) no-repeat`,
               [media.lessThan(`medium`)]: {
                 left: 0,
               },
@@ -188,12 +201,21 @@ const SupportPage = props => {
                 height: `100%`,
                 width: `1px`,
                 top: 0,
-                background: `linear-gradient(to bottom, ${colors.gray4}, ${colors.gray8}, ${colors.gray4})no-repeat`,
+                background: theme =>
+                  `linear-gradient(to bottom, ${theme.colors.gray4}, ${theme.colors.gray8}, ${theme.colors.gray4})no-repeat`,
               },
             },
           }}
         >
-          {neighbor}
+          <div>{neighbor}</div>
+
+          <DonateButton
+            variant="cta"
+            to="/de/spenden/"
+            outerStyles={{ m: `auto 0 0` }}
+          >
+            Jetzt spenden
+          </DonateButton>
         </div>
 
         <div
@@ -204,123 +226,84 @@ const SupportPage = props => {
           }}
         >
           <div>{support}</div>
-        </div>
-      </div>
 
-      <div
-        sx={{
-          position: `relative`,
-          marginBottom: `3rem`,
-          ...fullPageWidth,
-        }}
-      >
-        <div
-          sx={{
-            display: `flex`,
-            justifyContent: `flex-end`,
-            marginBottom: `0.5rem`,
-          }}
-        >
-          {
-            <Img
-              fluid={props.data.page.frontmatter.assets.parrots.image.fluid}
-              sx={{
-                transform: `translateY(3rem)`,
-                marginTop: `-3rem`,
-                width: `300px`,
-                [media.greaterThan(`medium`)]: {
-                  width: `450px`,
-                },
-                '& img': {
-                  margin: 0,
-                },
-              }}
-            />
-          }
-        </div>
-
-        <div
-          sx={{
-            backgroundColor: `lightBlue`,
-            display: `flex`,
-            justifyContent: `space-around`,
-            marginBottom: `5rem`,
-            '& > div': {
-              position: `relative`,
-              zIndex: `3`,
-              margin: `2.5rem 0`,
-              '& > a': {
-                color: `black`,
-                [media.lessThan(`medium`)]: {
-                  fontSize: `0.7rem`,
-                },
-              },
-            },
-          }}
-        >
-          {props.data.Accounts.frontmatter.accounts
-            .filter(x => x.meta !== `true`)
-            .map(x => (
-              <div
-                key={x.service}
-                sx={{
-                  '&:hover svg': {
-                    color: `${x.service !== `instagram` &&
-                      `brands.${x.service}`}`,
-                  },
-                  '&:hover svg *': {
-                    fill: `${x.service === `instagram` &&
-                      `url(#InstagramGradient)`}`,
-                  },
-                }}
-              >
-                <StyledA
-                  href={x.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title={x.description}
-                >
-                  <FontAwesomeIcon icon={[`fab`, x.icon]} size="3x" />
-                </StyledA>
-              </div>
-            ))}
           <div
             sx={{
-              '&:hover svg': {
-                color: `grayTurqoise`,
-              },
+              display: `flex`,
+              margin: `1rem -0.5rem 0`,
             }}
           >
-            <StyledA
-              href={`${props.data.page.frontmatter.contactLink}#Newsletter`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faNewspaper} size="3x" />
-            </StyledA>
+            {props.data.Accounts.frontmatter.accounts
+              .filter(x => x.meta !== `true`)
+              .map(x => (
+                <div
+                  key={x.service}
+                  sx={{
+                    mx: `0.5rem`,
+                    svg: {
+                      color: x.service !== `instagram` && x.service,
+                    },
+                    'svg *': {
+                      fill:
+                        x.service === `instagram` && `url(#InstagramGradient)`,
+                    },
+                    ':hover svg': {
+                      color: `black`,
+                      '*': {
+                        fill: `black`,
+                      },
+                    },
+                  }}
+                >
+                  <StyledA
+                    href={x.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title={x.description}
+                  >
+                    <FontAwesomeIcon
+                      icon={[x.icon.prefix, x.icon.name]}
+                      size="3x"
+                    />
+                  </StyledA>
+                </div>
+              ))}
           </div>
-          {/* <div
-            sx={{
-              '&:hover svg': {
-                color: `rss`,
-              }
-            }}
-          >
-            <StyledA
-              href={`/${props.pageContext.lang}/blog/rss.xml`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faRssSquare} size="3x" />
-            </StyledA>
-          </div> */}
         </div>
       </div>
 
+      <Box mt="5rem" bg={`background2`} p="2rem">
+        <Box mx="auto" sx={{ maxWidth: `50rem` }}>
+          <Heading id="contributors">{frontmatter.contributors.title}</Heading>
+          <Text mt="1rem">
+            {frontmatter.contributors.descr}
+            <Link
+              to={frontmatter.contributors.donateLink}
+              sx={{ ml: `0.5rem` }}
+            >
+              {frontmatter.contributors.donateLabel}
+            </Link>
+          </Text>
+          <ContributorList>
+            {contributors.edges.map(x => (
+              <Contributor key={x.node.item.key}>
+                {x.node.item.name}
+              </Contributor>
+            ))}
+          </ContributorList>
+          <ContributorListLink to={frontmatter.contributors.link}>
+            {frontmatter.contributors.linkLabel}
+          </ContributorListLink>
+        </Box>
+      </Box>
+
       <TitledCopy
-        centered
+        // centered
         title={frontmatter.getIdea.title}
         sx={{
+          mt: `5rem`,
+          mx: `auto`,
+          maxWidth: `50rem`,
           '& > h2': {
             marginBottom: `0.7rem`,
             [media.lessThan(`medium`)]: {
@@ -340,7 +323,9 @@ const SupportPage = props => {
 
       <div
         sx={{
-          margin: `3rem 0 3rem`,
+          mt: `2rem`,
+          mx: `auto`,
+          maxWidth: `50rem`,
           display: `flex`,
           [media.lessThan(`small`)]: {
             flexDirection: `column`,
@@ -419,94 +404,14 @@ const SupportPage = props => {
         ))}
       </div>
 
-      <div>
-        <ContributorListTitle id="contributors">
-          {frontmatter.contributors.title}
-        </ContributorListTitle>
-        <ContributorList>
-          {contributors.edges.map(x => (
-            <Contributor key={x.node.item.key}>{x.node.item.name}</Contributor>
-          ))}
-        </ContributorList>
-        <ContributorListLink to={frontmatter.contributors.link}>
-          {frontmatter.contributors.linkLabel}
-        </ContributorListLink>
-      </div>
-
-      <div
-        sx={{
-          marginTop: `4rem`,
-          backgroundColor: `lightBlue`,
-          position: `relative`,
-          padding: `2rem 1rem`,
-          ...fullPageWidth,
-        }}
-      >
-        <div
-          sx={{
-            [media.greaterThan(`small`)]: {
-              display: `flex`,
-              justifyContent: `center`,
-            },
-            '& > div': {
-              [media.greaterThan(`small`)]: {
-                width: `47%`,
-              },
-            },
-          }}
-        >
-          {[`left`].map(x => (
-            <div key={x}>
-              <h3
-                sx={{
-                  textAlign: `center`,
-                  fontSize: `1.8rem`,
-                }}
-              >
-                {frontmatter.checklists[x].title}
-              </h3>
-
-              <div
-                sx={{
-                  marginLeft: `3rem`,
-                  display: `flex`,
-                  flexDirection: `column`,
-                  alignItems: `space-between`,
-                  '& > div': {
-                    position: `relative`,
-                    fontSize: `0.9rem`,
-                  },
-                  '& > div:not(:last-child)': {
-                    marginBottom: `1.3rem`,
-                  },
-                  '& > div:before': {
-                    content: `url(${CheckMark})`, // TODO: dark-theme replace by CSS or SVG
-                    position: `absolute`,
-                    left: `-3rem`,
-                    top: `0.4rem`,
-                  },
-                  [media.lessThan(`small`)]: {
-                    '& > div:last-child': {
-                      marginBottom: `1.5rem`,
-                    },
-                  },
-                }}
-              >
-                {frontmatter.checklists[x].items.map((x, i) => (
-                  <div key={i}>{x}</div>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
       <TitledCopy
-        centered
+        // centered
         title={frontmatter.outro.title}
         paragraphs={frontmatter.outro.text}
         sx={{
-          margin: `5rem auto 0`,
+          mt: `5rem`,
+          mx: `auto`,
+          maxWidth: `50rem`,
           [media.lessThan(`medium`)]: {
             '& div': {
               fontSize: `0.9rem`,
@@ -619,18 +524,11 @@ export const query = graphql`
         }
         contributors {
           title
+          descr
+          donateLabel
+          donateLink
           link
           linkLabel
-        }
-        checklists {
-          left {
-            title
-            items
-          }
-          right {
-            title
-            items
-          }
         }
         outro {
           title
