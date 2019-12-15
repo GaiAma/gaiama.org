7f5a7c5ca0ccaecd2c0ae791ee4a7effbac770cd
CanRau <cansrau@gmail.com>
2019-12-15 12:55:26 -0500

    feat: update FA icon library

--- src/utils/fontawesome.js
+++ src/utils/fontawesome.js
@@ -1,10 +1,16 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import {
+import { faNewspaper } from '@fortawesome/free-solid-svg-icons/faNewspaper'
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons/faFacebookSquare'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons/faYoutube'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter'
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
+
+library.add(
   faFacebookSquare,
   faInstagram,
   faYoutube,
   faTwitter,
   faGithub,
-} from '@fortawesome/free-brands-svg-icons'
-
-library.add(faFacebookSquare, faInstagram, faYoutube, faTwitter, faGithub)
+  faNewspaper
+)
