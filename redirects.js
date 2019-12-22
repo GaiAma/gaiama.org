75ea3d8ccec6fb3fa89724d4232ad92993a41330
CanRau <cansrau@gmail.com>
2019-12-21 19:50:00 -0500

    feat: update redirects

--- redirects.js
+++ redirects.js
@@ -1,35 +1,42 @@
+const ifGermanLanguage = `Language=de,de-de,de-at,de-ch,cd-li,de-lu`
+
 module.exports = [
   // Redirect default Netlify subdomain to primary domain
+  `https://gaiama.netlify.com/* https://www.gaiama.org/de/:splat 301! ${ifGermanLanguage}`,
+  `https://gaiama.netlify.com/* https://www.gaiama.org/en/:splat 301!`,
   `https://gaiama.netlify.com/en/* https://www.gaiama.org/en/:splat 301!`,
   `https://gaiama.netlify.com/de/* https://www.gaiama.org/de/:splat 301!`,
-  `https://gaiama.netlify.com/* https://www.gaiama.org/en/:splat 301!`,
   // Proxy /api to api.gaiama.org
-  `/api/pixel/* https://pixel.api.gaiama.org/:splat 200`,
-  `/api/* https://api.gaiama.org/:splat 200`,
-  // subdomain redirects
-  `https://spende.gaiama.org/* https://www.gaiama.org/en/donate/?ref=spende.gaiama.org#main-nav 301! Language=en`,
-  `https://www.spende.gaiama.org/* https://www.gaiama.org/en/donate/?ref=spende.gaiama.org#main-nav 301! Language=en`,
-  `https://spende.gaiama.org/* https://www.gaiama.org/de/spenden/?ref=spende.gaiama.org#main-nav 301!`,
-  `https://www.spende.gaiama.org/* https://www.gaiama.org/de/spenden/?ref=spende.gaiama.org#main-nav 301!`,
-  `https://donate.gaiama.org/* https://www.gaiama.org/de/spenden/?ref=donate.gaiama.org#main-nav 301! Language=de`,
-  `https://www.donate.gaiama.org/* https://www.gaiama.org/de/spenden/?ref=donate.gaiama.org#main-nav 301! Language=de`,
-  `https://donate.gaiama.org/* https://www.gaiama.org/en/donate/?ref=donate.gaiama.org#main-nav 301!`,
-  `https://www.donate.gaiama.org/* https://www.gaiama.org/en/donate/?ref=donate.gaiama.org#main-nav 301!`,
+  // `/api/pixel/* https://pixel.api.gaiama.org/:splat 200`,
+  // `/api/* https://api.gaiama.org/:splat 200`,
 
-  `https://www.shop.gaiama.org/* https://www.gaiama.org/de/shop/ 301! Language=de`,
+  // subdomain redirects
+  `https://spende.gaiama.org/* https://www.gaiama.org/de/spenden/ 301! ${ifGermanLanguage}`,
+  `https://www.spende.gaiama.org/* https://www.gaiama.org/de/spenden/ 301! ${ifGermanLanguage}`,
+  `https://spende.gaiama.org/* https://www.gaiama.org/en/donate/ 301!`,
+  `https://www.spende.gaiama.org/* https://www.gaiama.org/en/donate/ 301!`,
+  `https://donate.gaiama.org/* https://www.gaiama.org/de/spenden/ 301! ${ifGermanLanguage}`,
+  `https://www.donate.gaiama.org/* https://www.gaiama.org/de/spenden/ 301! ${ifGermanLanguage}`,
+  `https://donate.gaiama.org/* https://www.gaiama.org/en/donate/ 301!`,
+  `https://www.donate.gaiama.org/* https://www.gaiama.org/en/donate/ 301!`,
+
+  // TODO: move redirects to the respective MDX files, how about Language wildcards?
+  `https://shop.gaiama.org/de https://www.gaiama.org/de/shop/ 301! ${ifGermanLanguage}`,
+  `https://shop.gaiama.org/en https://www.gaiama.org/en/shop/ 301!`,
+  `https://www.shop.gaiama.org/* https://www.gaiama.org/de/shop/ 301! ${ifGermanLanguage}`,
   `https://www.shop.gaiama.org/* https://www.gaiama.org/en/shop/ 301!`,
-  `https://shop.gaiama.org/* https://www.gaiama.org/de/shop/ 301! Language=de`,
+  `https://shop.gaiama.org/* https://www.gaiama.org/de/shop/ 301! ${ifGermanLanguage}`,
   `https://shop.gaiama.org/* https://www.gaiama.org/en/shop/ 301!`,
 
-  `/shop /de/shop/ 301 Language=de`,
+  `/shop /de/shop/ 301 ${ifGermanLanguage}`,
   `/shop /en/shop/ 301`,
 
   // donation shortcuts
-  `/spenden /de/spenden/?ref=gaiama.org/spenden#main-nav 301`,
-  `/spende /de/spenden/?ref=gaiama.org/spende#main-nav 301`,
-  `/donate /en/donate/?ref=gaiama.org/donate#main-nav 301`,
+  `/spenden /de/spenden/ 301`,
+  `/spende /de/spenden/ 301`,
+  `/donate /en/donate/ 301`,
 
-  `/de/blog* /de 301 Language=de`,
+  `/de/blog* /de 301 ${ifGermanLanguage}`,
   `/en/blog* /en 301`,
 
   // `/Left-in-the_Dark_free-edition-c604d4d35b6aedd11d4470a8cee465fd.pdf /assets/Left-in-the_Dark_free-edition.pdf 301`,
