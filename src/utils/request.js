1a88ef5f83cf9b602d66008594342e7d2c644078
CanRau <cansrau@gmail.com>
2020-09-20 14:37:36 -0500

    feat: add request util

---
+++ src/utils/request.js
@@ -0,0 +1,34 @@
+const defaultHeaders = {
+  'content-type': 'application/x-www-form-urlencoded',
+  accept: 'application/json',
+}
+
+// from https://ccoenraets.github.io/es6-tutorial-data/promisify/
+// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+export const request = obj => {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest()
+    xhr.open(obj.method || 'GET', obj.url)
+    const headers = Object.assign({}, defaultHeaders, obj.headers)
+    if (headers) {
+      Object.keys(headers).forEach(key => {
+        xhr.setRequestHeader(key, headers[key])
+      })
+    }
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response)
+      } else {
+        reject({ status: xhr.status, message: xhr.statusText })
+      }
+    }
+    xhr.onerror = () => reject(xhr.statusText)
+    xhr.send(obj.body)
+  })
+}
+
+request.post = (url, body, headers) =>
+  request({ method: 'POST', url, body, headers })
+
+request.postJson = (url, body, headers) =>
+  request({ method: 'POST', url, headers, body: JSON.stringify(body) })
