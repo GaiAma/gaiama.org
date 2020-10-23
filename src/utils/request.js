de17ab2a374d1a65246c0b7fa216a485bc8918c8
CanRau <cansrau@gmail.com>
2020-10-23 17:17:29 -0500

    feat(request): resolve all HTTP status codes

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,22 +5,22 @@ const defaultHeaders = {
 
 // from https://ccoenraets.github.io/es6-tutorial-data/promisify/
 // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
-export const request = obj =>
+export const request = (obj) =>
   new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest()
     xhr.open(obj.method || `GET`, obj.url)
     const headers = Object.assign({}, defaultHeaders, obj.headers)
     if (headers) {
-      Object.keys(headers).forEach(key => {
+      Object.keys(headers).forEach((key) => {
         xhr.setRequestHeader(key, headers[key])
       })
     }
     xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.response)
-      } else {
-        reject({ status: xhr.status, message: xhr.statusText })
-      }
+      // if (xhr.status >= 200 && xhr.status < 300) {
+      resolve(xhr.response)
+      // } else {
+      //   reject({ status: xhr.status, message: xhr.statusText })
+      // }
     }
     xhr.onerror = () => reject(xhr.statusText)
     xhr.send(obj.body)
