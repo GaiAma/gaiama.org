e470ffbba9761b2dbee37d9b3b46c9b7cf222619
CanRau <cansrau@gmail.com>
2021-12-29 08:27:55 -0500

    fix: 2 coding4 redirects

--- redirects.js
+++ redirects.js
@@ -9,6 +9,8 @@ module.exports = [
   // `/api/* https://api.gaiama.org/:splat 200`,
 
   // coding4gaiama
+  `https://www.coding4.gaiama.org/en/fluid-typography https://www.canrau.com/en/css-only-fluid-typography 301!`,
+  `https://www.coding4.gaiama.org/en/graphql-parent-file https://www.canrau.com/en/query-parent-file-node-in-gatsby-js 301!`,
   `https://coding4.gaiama.org/* https://www.canrau.com/:splat 301!`,
   `https://www.coding4.gaiama.org/* https://www.canrau.com/:splat 301!`,
 
